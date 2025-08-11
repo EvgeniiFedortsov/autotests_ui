91e61fb4ab9ef73679fb77aca59b1cc62aa1c0c3
EvgeniiFedortsov <evgeniifedortsov@mail.kz>
2025-08-11 10:09:12 +0500

    5.4 Введение в Playwright.Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,7 +1,9 @@
-# Открыть страницу https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login
-# Проверить наличие элементов: "Email", "Password", "Login"
-# Нажать на ссылку "Registration", после чего произойдет редирект на страницу Registration
-# Проверить наличие элементов: "Email", "Password", "Registration"
+# 1.Откроет страницу https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration
+# 2.Заполнит поле "Email" значением "user.name@gmail.com"
+# 3.Заполнит поле "Username" значением "username"
+# 4.Заполнит поле "Password" значением "password"
+# 5.Нажмет на кнопку "Registration". После нажатия кнопки "Registration" произойдет редирект на страницу "Dashboard"
+# 6.Проверит, что на странице "Dashboard" отображается заголовок "Dashboard"
 
 
 # Импорт Playwright для синхронного режима и проверки
@@ -13,43 +15,38 @@ with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()  # Создаем новую страницу
 
-    # Переходим на страницу авторизации
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    # 1.Открываем страницу регистрации
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    # Проверяем наличие элементов: "Email", "Password", "Login" на странице авторизации
 
-    # Находим поле "Email" и проверяем видимость
-    login_email_input = page.get_by_test_id('login-form-email-input').locator('input')
-    expect(login_email_input).to_be_visible()
-
-    # Находим поле "Password" и проверяем видимость
-    login_password_input = page.get_by_test_id('login-form-password-input').locator('input')
-    expect(login_password_input).to_be_visible()
-
-    # Находим кнопку "Login" и проевряем видимость
-    login_button = page.get_by_test_id('login-page-login-button')
-    expect(login_button).to_be_visible()
-
-    # Пауза на 5 секунд, чтобы увидеть результат
-    page.wait_for_timeout(5000)
-
-    # Нажимаем на ссылку "Registration"
-    registration_link = page.get_by_test_id("login-page-registration-link")
-    registration_link.click()
-
-    # Проверяем наличие элементов: "Email", "Password", "Registration" на странице регистрации
-
-    # Проверяем наличие элементов: "Email"
+    # 2. Проверяем наличие элементов: "Email" и заполняем данными
     registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     expect(registration_email_input).to_be_visible()
+    registration_email_input.fill("user.name@gmail.com")
 
-    # Проверяем наличие элементов: "Password"
+    # 3. Проверяем наличие элементов: "Username" и заполняем данными
+    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    expect(registration_username_input).to_be_visible()
+    registration_username_input.fill("username")
+
+    # 4. Проверяем наличие элементов: "Password"
     registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
     expect(registration_password_input).to_be_visible()
+    registration_password_input.fill("password")
 
-    # Проверяем наличие элементов: "Registration"
+    # 5. Проверяем наличие элементов: "Registration" и нажимаем кнопку
     registration_button = page.get_by_test_id('registration-page-registration-button')
     expect(registration_button).to_be_visible()
+    registration_button.click()
+
+
+
+    # 6. Проверяем наличие элементов: "Dashboard"
+    dashboard_page = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_page).to_be_visible()
+    expect(dashboard_page).to_have_text("Dashboard")
+
+
 
 
     # Пауза на 5 секунд, чтобы увидеть результат

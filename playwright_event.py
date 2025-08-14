4f057950ca6c8bfd9ad965ebd51ae52c820749ab
EvgeniiFedortsov <evgeniifedortsov@mail.kz>
2025-08-14 23:02:08 +0500

    Углубление в playwright

--- playwright_event.py
+++ playwright_event.py
@@ -1,10 +1,8 @@
 from playwright.sync_api import sync_playwright, Request, Response
 
-
 # Логирование запросов
 def log_request(request: Request):
     print(f"Request: {request.url}")
-
 
 # Логирование ответов
 def log_response(response: Response):
